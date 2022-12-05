1867ff4e5ca6efeeb4e833c44d89a2b639c7a4b2
nielsperdon <niels.perdon@gmail.com>
2022-12-05 10:44:58 +0100

    first commit

---
+++ src/Email.java
@@ -0,0 +1,23 @@
+public class Email {
+     private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternative;
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Ask department
+
+    // generate random password
+
+    // set mailbox capacity
+
+    // set alternate email
+
+    // Change password
+}
